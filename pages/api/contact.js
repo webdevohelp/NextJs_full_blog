856f137d8f496841244384963a2733d4cd42934e
Web Dev Help <43485053+webdevohelp@users.noreply.github.com>
2022-02-27 15:50:56 +0530

    connected contact us to database
    connected contact us to database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,9 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb';
+const dbUrl =
+    'mongodb://localhost:27017/blog?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+const dbName = 'blog';
+
+export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { email, name, message } = req.body;
         if (
@@ -12,12 +17,35 @@ export default function handler(req, res) {
             res.status(422).json({ message: 'Invalid input' });
             return;
         }
+
         // Store in a db
         const newMessage = {
             email,
             name,
             message,
         };
+
+        // declaring client here to resolve scoping problems
+        let client;
+        try {
+            client = await MongoClient.connect(dbUrl);
+            console.log('connected to the server');
+        } catch (error) {
+            res.status(500).json({ message: 'Unable to connect to database.' });
+            return;
+        }
+
+        const db = client.db();
+        try {
+            const result = await db
+                .collection('messages')
+                .insertOne(newMessage);
+            newMessage.id = result.insertedId;
+        } catch (error) {
+            client.close();
+            res.status(500).json({ message: 'unable to save message' });
+        }
+        client.close();
         console.log(newMessage);
         res.status(201).json({ message: 'Success', data: newMessage });
     }
